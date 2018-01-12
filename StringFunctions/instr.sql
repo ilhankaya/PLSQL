ff4378b93c52813d0371274a6a4ed682aeab85e1
ilhan.kaya <ilhan.kaya2010@gmail.com>
2018-01-12 11:00:46 +0300

    instr function

--- StringFunctions/instr.sql
+++ StringFunctions/instr.sql
@@ -1,6 +1,11 @@
 -- Description : The Oracle/PLSQL INSTR function returns the location of a substring in a string.
 -- Syntax : INSTR( string, substring [, start_position [, th_appearance ] ] )
 
+-- H e l l o   W o r l  d
+-- 1 2 3 4 5 6 7 8 9 10 11
+--   2                     : 'e' position
+--         5     8         : 'o' position
+
 select instr('Hello World'
             ,'e')
   from dual;
