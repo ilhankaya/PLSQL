359f0156e1a0f0f9acd6e08430c50bbfe2e71eeb
ilhan.kaya <ilhan.kaya2010@gmail.com>
2018-01-12 11:00:57 +0300

    instr2 function

---
+++ StringFunctions/instr2.sql
@@ -0,0 +1,40 @@
+-- Description : The Oracle/PLSQL INSTR2 function returns the location of a substring in a string, using UCS2 code points.
+-- Syntax : INSTR2( string, substring [, start_position [, th_appearance ] ] )
+
+-- S t r i n g   F u n  c  t  i  o  n  s
+-- 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+--         1         2              3     : instr('String Functions', 'n', 1 [,th_appearance])
+--         3         2              1     : instr('String Functions', 'n', -1 [,th_appearance])
+
+select instr2('String Functions'
+             ,'n')
+  from dual;
+-- Result : 5
+
+select instr2('String Functions'
+             ,'n'
+             ,1
+             ,1)
+  from dual;
+-- Result : 5
+
+select instr2('String Functions'
+             ,'n'
+             ,1
+             ,2)
+  from dual;
+-- Result : 10
+
+select instr2('String Functions'
+             ,'n'
+             ,6
+             ,2)
+  from dual;
+-- Result : 15
+
+select instr2('String Functions'
+             ,'n'
+             ,-1
+             ,2)
+  from dual;
+-- Result : 5
